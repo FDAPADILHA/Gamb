8deb857f417b9968f392b170549627550b5bc82b
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2021-12-19 22:26:01 -0300

    criacao da tela de login, main activity, criacao da activity_cadastro, instanciando e iniciando as primeiras funcoes dos botoes; adicao do arquivo politicas para uso da funcao power

--- app/src/main/java/br/com/filnat/gamb/MainActivity.java
+++ app/src/main/java/br/com/filnat/gamb/MainActivity.java
@@ -2,13 +2,54 @@ package br.com.filnat.gamb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity{
+
+    private EditText editTextLogin, editTextSenhaLogin;
+    private Button buttonCadastre;
+    private Button buttonAcessar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextLogin = findViewById(R.id.editTextLogin);
+        editTextSenhaLogin = findViewById(R.id.editTextSenhaLogin);
+        buttonCadastre = findViewById(R.id.buttonCadastre);
+        buttonAcessar = findViewById(R.id.buttonAcessar);
+
+        setButtonAcessar();
+        setButtonCadastre();
     }
+
+    public void setButtonCadastre() {
+
+        buttonCadastre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void setButtonAcessar() {
+
+        buttonAcessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
 }
